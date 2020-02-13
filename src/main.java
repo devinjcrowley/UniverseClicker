92a84015d2024c411ade008fcc2229bcd2867584
emmillet <hellosunshinq@gmail.com>
2020-02-13 17:47:39 -0500

    Imports

--- src/main.java
+++ src/main.java
@@ -1,11 +1,28 @@
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         GridPane grid = new GridPane();
+
+        Scene scene = new Scene(grid, 500, 500);
+        primaryStage.setTitle("Snake");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        grid.requestFocus();
     }
 }
 
