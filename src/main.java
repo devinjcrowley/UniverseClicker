c8fe2821821a474c87ea53662ab5d121cc3ed982
Devin <devinjcrowley@gmail.com>
2020-02-13 17:42:22 -0500

    Initial creation main class and instantiation of GridPaine.

---
+++ src/main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GridPane grid = new GridPane();
+    }
+}
+
