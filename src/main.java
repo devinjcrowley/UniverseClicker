876eda609307282f9f3e68bc26ca4a9a9d02efba
Devin <devinjcrowley@gmail.com>
2020-02-13 19:42:33 -0500

    First Steps to Create the Board

--- src/main.java
+++ src/main.java
@@ -9,20 +9,70 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class main extends Application {
+
+    // Instantiation of Start Method
     @Override
     public void start(Stage primaryStage) throws Exception {
-        GridPane grid = new GridPane();
 
-        Scene scene = new Scene(grid, 500, 500);
-        primaryStage.setTitle("Snake");
+        // Creation of Screen Grid
+        GridPane gridOrganizer = new GridPane();
+
+        // Creation of 4 Sub-grids
+        StackPane clickerPane = new StackPane();
+        GridPane progressPane = new GridPane();
+        Pane storeNamePane = new Pane();
+        GridPane storeShopsPane = new GridPane();
+
+        // Setting size of each grids' sizes.
+        gridOrganizer.getColumnConstraints().add(new ColumnConstraints(800));
+        gridOrganizer.getColumnConstraints().add(new ColumnConstraints(400));
+        gridOrganizer.getRowConstraints().add(new RowConstraints(150));
+        gridOrganizer.getRowConstraints().add(new RowConstraints(650));
+
+
+        // Clicker Area
+
+        //TODO:
+
+
+        // Progress Area
+
+        //TODO:
+
+        // Store Name Area
+
+        Text storeName = new Text("Lunar Loot");
+        storeName.setFont(Font.font("Calibiri", 30));
+        storeName.setFill(Color.WHITE);
+        storeNamePane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
+        storeNamePane.getChildren().add(storeName);
+        storeName.relocate(70, 20);
+
+
+        // Shops Area
+
+        //TODO:
+
+
+        // Grid Area
+
+        gridOrganizer.add(storeNamePane, 1, 0);
+
+
+        // Establishing setup of Window
+        Scene scene = new Scene(gridOrganizer, 1200, 800);
+        primaryStage.setTitle("Cosmos Clicker");
         primaryStage.setScene(scene);
         primaryStage.show();
 
-        grid.requestFocus();
+        // Ensures Animations Update
+        gridOrganizer.requestFocus();
     }
 }
 
